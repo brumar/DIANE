15a93a1216b7670d92737bae465bac22b9c6b51c
brumar <martin.bruno.mail@gmail.com>
2014-10-12 15:57:16 +0200

    delete useless line

--- encodingStuff.py
+++ encodingStuff.py
@@ -12,13 +12,11 @@ def reincodeMyFile(fullpath,filename):
             sys.exit()
         if(enc!="utf-8"):
             print(fullpath, " previously encoded in ", enc)
-            BLOCKSIZE = 1048576 # or some other, desired size in bytes
             sourceFile=codecs.open(fullpath, "r", enc)
             sourceString=sourceFile.read()
             sourceFile.close()
             with codecs.open(fullpath, "w", "utf-8") as targetFile:
-                targetFile.write(sourceString)
-            
+                targetFile.write(sourceString)           
 
 def walkToEncode():
     for root, dirs, files in os.walk("."):
