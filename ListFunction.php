ef0a174961d751af6e732e9c8260da1aca53f06b
Timothée Behra <timotheebehra.etu@gmail.com>
2015-07-10 13:07:38 +0200

    Ajout de fonctions autour de la vérification des contraintes numériques en orienté objet (classes Constraint et Expression)

--- ListFunction.php
+++ ListFunction.php
@@ -1,6 +1,8 @@
 <?php
 
-// Fonctions pour les listes
+//--------------------------------------------------------------------------------------------------------------------
+//								  					Gestion des listes
+//--------------------------------------------------------------------------------------------------------------------
 
 function loadList($type, $b){
 	$t=array();
@@ -28,7 +30,9 @@ function updateList($type, $new, $b){
 
 
 
-//BDD Functions
+//--------------------------------------------------------------------------------------------------------------------
+//									  	Fonctions de gestion de la base de données
+//--------------------------------------------------------------------------------------------------------------------
 
 function count_BDD($SQL_req, $array_req, $b){
 	$r = $b->prepare($SQL_req);
@@ -196,6 +200,276 @@ function creerSessionPassation($idEleve, $serie, $b, $type_passation){
 	$req->closeCursor();			
 	$_SESSION['passation']['totalExo'] = count($_SESSION['passation']['allProblems']);
 }
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------------
+//								  Gestion des classes "Contrainte" et "Opérateur"
+//--------------------------------------------------------------------------------------------------------------------
+
+
+function findComparisonOperator($string){
+	$operators_2char = array("<=", ">=");
+	foreach($operators_2char as $op){
+		if (strpos($string, $op) !== false){
+			return $op;
+		}
+	}
+	$operators = array("=", "<", ">");
+	foreach($operators as $op){
+		if (strpos($string, $op) !== false){
+			return $op;
+		}
+	}
+	return null;
+}
+
+class Constraint{
+	var $l;  //left expression
+	var $r;  //right expression
+	var $op; // comparison operator
+	
+	function Constraint($left_expression, $right_expression, $operator){
+		$this->l = $left_expression;
+		$this->r = $right_expression;
+		$this->op = $operator;
+	}
+
+	function isSatisfied($args){
+		// Args doit être un array de type array("Nombre1" => "3", "Nombre2" => "12", ...); 
+
+		switch($this->op){
+			case "=":
+				return (int)($this->l->evaluate($args) == $this->r->evaluate($args));
+				break;
+			case "<":
+				return (int)($this->l->evaluate($args) < $this->r->evaluate($args));
+				break;
+			case ">":
+				return (int)($this->l->evaluate($args) > $this->r->evaluate($args));
+				break;
+			case ">=":
+				return (int)($this->l->evaluate($args) >= $this->r->evaluate($args));
+				break;
+			case "<=":
+				return (int)($this->l->evaluate($args) <= $this->r->evaluate($args));
+				break;
+			default:
+				return null;
+		}
+	}
+
+	function tprint(){
+		echo $this->l->eprint()." ".$this->op." ".$this->r->eprint();
+	}
+}
+
+
+class Expression{
+	var $type;
+	var $contenu;
+
+	function Expression($t, $c){
+		if($t == "variable" || $t == "value"){
+			$this->type = $t;
+			if(is_numeric($c)){
+				$this->contenu = $c;
+			}
+			else{
+				$this->contenu = null;
+			}
+		}
+		elseif($t == "addition"){
+			$this->type = $t;
+			$this->contenu = $c;
+		}
+		else{
+			$this->type = null; //suremnet pas ce que je veux
+		}
+	}
+
+	function evaluate($args = null){
+		// return an int, args sent fill the variables.
+		if($this->type == "value"){
+			return $this->contenu;
+		}
+		elseif($this->type == "variable"){
+			$index = "Nombre".(string) $this->contenu;
+			return $args[$index]; //TODO : try catch here
+
+		}
+		elseif($this->type == "addition"){
+			$sum = 0;
+
+			for($i=0; $i<count($this->contenu["types"]); $i++){
+				if ($this->contenu["types"][$i] == "add_value"){
+					$sum = $sum + $this->contenu["contenus"][$i];
+				}
+				elseif ($this->contenu["types"][$i] == "add_variable"){
+					$index = "Nombre".$this->contenu["contenus"][$i];
+					$sum = $sum + $args[$index];
+				}
+				else{
+					return null;
+				}
+			}
+			return $sum;
+		}
+	}
+
+	function eprint(){ // affiche l'expression.
+		if($this->type == "variable"){
+			return "Nombre".$this->contenu;
+		}
+		elseif($this->type == "value"){
+			return (string)$this->contenu;	
+		}
+		elseif($this->type == "addition"){
+			$concat = "";
+			$first_iteration = true;
+			for($i = 0; $i<count($this->contenu["types"]); $i++){
+				if($first_iteration){
+					$first_iteration = false;
+				}
+				else{
+					$concat = $concat."+";
+				}
+				if ($this->contenu["types"][$i] == "add_value"){
+					$concat = $concat.(string)$this->contenu["contenus"][$i];
+				}
+				elseif ($this->contenu["types"][$i] == "add_variable"){
+					$concat = $concat."Nombre".$this->contenu["contenus"][$i] ;
+				}
+				else{
+					return null;
+				}
+			}
+			return $concat;
+		}
+	}
+}
+
+function evalExpression($string){
+	// Créer une instance de la classe Expression à partir d'une chaine de caractères
+	
+	// Tout d'abord, on refuse l'expression si elle contient un caractère interdit
+
+	$forbiddenChars = array("/", "-", "*", "=", "<", ">", ">=", "<=");
+	foreach($forbiddenChars as $ch){
+		if (strpos($string, $ch) !== false){
+			return null;
+		}
+	} 
+
+	/* L'expression peut être soit : 
+		- une variable => type "variable"
+		- un nombre => type "value"
+		- une expression avec un ou plusieurs "+" => type "addition" */
+
+	if(strpos($string, "+") !== false){ // Addition
+		// Dans ce cas, je veux que expression renvoie : type = "addition", contenu = array(types, contenus)
+		$regex_variable =  '#^n(ombre)?[1-9]$#i';
+		$tmp = explode("+", $string);
+		$typeTab = [];
+		$contentTab = [];
+		foreach($tmp as $elem){
+			if(!($elem)) {
+				return null;
+			}
+			elseif(is_numeric($elem)){
+				$curType = "add_value";
+				$curContent = (int)$elem;
+			}
+			else{
+				if(preg_match($regex_variable, $elem)){
+					$curType = "add_variable";
+					$curContent = (int)mb_substr($elem, -1); //On récupère le chiffre, qui est le dernier caractère
+				}
+				else{
+					return null;
+				}
+			}
+			$typeTab[] = $curType;
+			$contentTab[] = $curContent;
+		}
+		$totalContent = array("types" => $typeTab, "contenus" => $contentTab);
+		$expr = new Expression('addition', $totalContent);	
+	}
+	elseif(is_numeric($string)){ //Nombre
+		$expr = new Expression('value', (int)$string);	
+	}
+	else{ //Variable au format "NombreX"
+		$regex_variable =  '#^n(ombre)?[1-9]$#i'; // Pour l'instant, on accepte uniquement Nombre1, nombre1, NOMBRE1, n1, N1...
+		if(preg_match($regex_variable, $string)){
+			$var_number = mb_substr($string, -1); //On récupère le chiffre, qui est le dernier caractère
+			$expr = new Expression('variable', (int)$var_number);
+		}
+		else{
+			return null;
+		}
+	}
+	return $expr;
+}
+
+function parseNumericConstraints($string){
+	// Renvoie un tableau de contraintes à partir d'une chaine de caractères
+	$constraints = array();
+
+	// Format d'une contrainte :  EXPRESSION1 COMPARISON_OPERATOR EXPRESSION2
+	// EXPRESSION : VAR, ou VAR+VAR
+	// VAR : (VALUE) OU (NombreX where X = VALUE, with VALUE <= $compteur_nombre)
+	// VALUE doit être un int
+	// COMPARISON_OPERATOR : <, >, =, >=, <=
+
+	foreach(explode(";",$string) as $constraint){
+		$constraint = str_replace(" ", "", $constraint); // On supprime tous les espaces de la string
+		$constraint = trim($constraint);
+		$comparison_operator = findComparisonOperator($constraint);
+		if($comparison_operator){
+			$tmp = explode($comparison_operator, $constraint);
+			if(count($tmp)==2){
+				if(!($left = evalExpression($tmp[0]))) {
+					return null;
+				}
+				if(!($right = evalExpression($tmp[1]))) {
+					return null;
+				}
+			}
+			else{ // L'opérateur de comparaison est présent deux fois
+				return null;
+			}
+		}
+		else{ // Pas d'opérateur de comparaison
+			return null;
+		}
+
+		$constraints[] = new Constraint($left, $right, $comparison_operator);
+	}
+	return $constraints;
+}
+
+
+
+
+
+
+//--------------------------------------------------------------------------------------------------------------------
+//								  Fonctions qui utilisent javascript en PHP...
+//--------------------------------------------------------------------------------------------------------------------
+
+function alertPHP($message){
+	echo "<script type=\"text/javascript\">
+		window.onload=function(){
+		alert(\"".$message."\");
+		}
+		</script>";
+}
+
+
+
+
 
 
 ?>
